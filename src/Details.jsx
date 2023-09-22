44a93ccc19f71227dd1ae6f372f2902f1eae3ea5
Bayardo Lopez <bjlslopez@gmail.com>
2023-09-22 12:59:56 -0600

    header

--- src/Details.jsx
+++ src/Details.jsx
@@ -1,3 +1,8 @@
-const Details = () => <h2>hi!</h2>;
+import { useParams } from "react-router-dom";
+
+const Details = () => {
+  const { id } = useParams();
+  return <h2>{id}</h2>;
+};
 
 export default Details;
